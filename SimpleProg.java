9c5604670bf3d076738881ef723d0b9450519d9b
nehanarta <nehabaswanthrao@gmail.com>
2023-06-16 08:26:53 +0530

    Added:first program

---
+++ SimpleProg.java
@@ -0,0 +1,10 @@
+package com.bridglabz;
+
+public class SimpleProg {
+    public static void main(String[] args){
+        String str="Welcome to BridgeLabZ";
+        System.out.println(str);
+
+
+    }
+}
